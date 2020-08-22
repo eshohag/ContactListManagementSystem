418293f80a57dcfa00bce3c33cd5182e1af54c82
MD Shohag Mia <eshohag@outlook.com>
2020-08-22 23:32:25 +0600

    Add project files.

---
+++ ContactListApi/Models/ContactListContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListApi.Models
+{
+    public class ContactListContext : DbContext
+    {
+        public ContactListContext(DbContextOptions<ContactListContext> options) : base(options)
+        {
+        }
+        public DbSet<ContactListItem> ContactListItems { get; set; }
+    }
+}
---
+++ ContactListApi/Models/ContactListItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactListApi.Models
+{
+    public class ContactListItem
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
